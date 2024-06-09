bed25070863635966d2331188f52e9ecaf241f81
Zoubaida Chatti <zoubaidachatti@outlook.com>
2024-06-09 12:08:40 +0100

    "Remove background styles from index.css and move to globalStyles.tsx, also remove debugging code"

--- src/theme/globalStyles.tsx
+++ src/theme/globalStyles.tsx
@@ -5,29 +5,15 @@ import Stars from '../assets/images/bg-stars.svg';
 
 export default function GlobalStyles() {
   const theme = useTheme();
-  // Debugging to check if the image paths are correct
-  console.log('Hills:', Hills);
-  console.log('Stars:', Stars);
 
-  // Function to check if images are loaded correctly
-  const checkImage = (url: string) => {
-    const img = new Image();
-    img.src = url;
-    img.onload = () => console.log(`${url} loaded successfully`);
-    img.onerror = () => console.error(`Error loading ${url}`);
-  };
-
-  checkImage(Hills);
-  checkImage(Stars);
   return (
     <GlobalThemeStyles
       styles={{
         body: {
           height: '100vh',
-          background: `url(${Hills}) no-repeat bottom right 20%, url(${Stars}), linear-gradient(to bottom, ${theme.palette.grey[900]}, ${theme.palette.primary.dark})`,
+          background: `url(${Hills}) no-repeat bottom right 20%, url(${Stars}), linear-gradient(to bottom, ${theme.palette.grey[900]}, ${theme.palette.primary.dark}) !important`,
           overflowX: 'hidden',
-          transition: 'background-color  .2s linear',
-          backgroundSize: '100%',
+          backgroundSize: '100%, cover',
         },
       }}
     />
