0d3136782912cf9ffce00e558f11051a7ad1238d
Zoubaida Chatti <zoubaidachatti@outlook.com>
2024-06-09 11:45:06 +0100

    Remove Hills and Stars image imports and references from GlobalStyles component.

--- src/theme/globalStyles.tsx
+++ src/theme/globalStyles.tsx
@@ -1,7 +1,5 @@
 import { GlobalStyles as GlobalThemeStyles } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
-import Hills from '@/assets/images/pattern-hills.svg';
-import Stars from '@/assets/images/bg-stars.svg';
 
 export default function GlobalStyles() {
   const theme = useTheme();
@@ -10,8 +8,7 @@ export default function GlobalStyles() {
       styles={{
         body: {
           height: '100vh',
-          background: `url(${Hills}) no-repeat bottom right 20%, url(${Stars}),
-                      linear-gradient(to bottom, ${theme.palette.grey[900]}, ${theme.palette.primary.dark})`,
+          background: `linear-gradient(to bottom, ${theme.palette.grey[900]}, ${theme.palette.primary.dark})`,
           overflowX: 'hidden',
           transition: 'background-color  .2s linear',
           backgroundSize: '100%',
